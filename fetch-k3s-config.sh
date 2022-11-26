03dd7a2530b01f0b94659167238b057836ef8e7e
King XORSE <75050740+King-XORSE@users.noreply.github.com>
2022-11-26 21:56:26 +0330

    Update fetch-k3s-config.sh

--- fetch-k3s-config.sh
+++ fetch-k3s-config.sh
@@ -4,7 +4,3 @@ echo "Enter Docker registry username: "
 read USERNAME
 echo "Enter Docker registry password: "
 read PASSWORD
-
-docker login -u ${USERNAME} --password-stdin ${PASSWORD} registry.23alice.duckdns.org
-
-curl https://minio.23alice.duckdns.org/config/config > ~/.kube/config
